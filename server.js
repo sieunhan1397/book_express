c150ce4c1f9e378b4e14f1c3431914862f1b70e1
Nhan <sieunhan1397@gmail.com>
2020-06-05 11:58:49 +0700

    Add Author Routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 const expressLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
@@ -12,6 +13,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -22,4 +24,5 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 app.listen(process.env.PORT || 3000);
